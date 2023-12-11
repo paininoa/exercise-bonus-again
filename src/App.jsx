5c1702ea16ccfbf851457af79c601e7b4212ab2a
Alessio <carninoalessio@gmail.com>
2023-12-11 17:49:04 +0100

    EOD 11/12

--- src/App.jsx
+++ src/App.jsx
@@ -34,10 +34,78 @@ import { useState } from "react";
 import "./App.css";
 import CustomFormElement from "./CustomFormElement/CustomFormElement";
 
+const formList = [
+  {
+    id: "first-name",
+    label: "First Name",
+    value: "",
+    mode: "input",
+  },
+  {
+    id: "last-name",
+    label: "Last Name",
+    value: "",
+    mode: "input",
+  },
+  {
+    id: "sex",
+    label: "Sex",
+    value: "",
+    mode: "radio",
+    options: ["m", "f"],
+  },
+  {
+    id: "color",
+    label: "Color",
+    value: "blue",
+    mode: "select",
+    options: ["blue", "green", "yellow"],
+  },
+  {
+    id: "newsletter",
+    label: "Newsletter",
+    value: false,
+    mode: "checkbox",
+  },
+];
 function App() {
+  const [formData, setFormData] = useState(formList);
+
+  const handleClick = () => {
+    return (
+      <ul>
+        {formData.map((obj, ix) => {
+          <li key={`li${ix}`}>{obj.value}</li>;
+        })}
+      </ul>
+    );
+  };
+
   return (
     <>
-      <CustomFormElement mode="input" label="Your Name" />
+      <div>
+        {formData.map((obj, objIndex) => {
+          const onChangeVal = obj.mode === "checkbox" ? "checked" : "value";
+
+          return (
+            <CustomFormElement
+              key={obj.id}
+              mode={obj.mode}
+              label={obj.label}
+              value={obj.value}
+              onChange={(e) => {
+                setFormData(() => {
+                  const newFormData = structuredClone(formData);
+                  newFormData[objIndex].value = e.target[onChangeVal];
+                  return newFormData;
+                });
+              }}
+            />
+          );
+        })}
+
+        <button onClick={handleClick}>Submit</button>
+      </div>
     </>
   );
 }
--- src/CustomFormElement/CustomFormElement.jsx
+++ src/CustomFormElement/CustomFormElement.jsx
@@ -1,21 +1,49 @@
 import { useState } from "react";
 
-export default function ({ mode, label, value, onChange, id, options }) {
-  //   formData[(Array, setArray)] = useState({});
-
+export default function ({ mode, label, value, id, options, onChange }) {
   switch (mode) {
     case "input":
-      return <input type="text" label={label} />;
-    case "select":
-      return <select></select>;
-    case "checkbox":
-      return <input type="checkbox" />;
-    case "radio":
       return (
-        <div>
-          <input type="radio" />
-          <input type="radio" />
-        </div>
+        <label>
+          <input type="text" id={id} value={value} onChange={onChange} />
+          {label}
+        </label>
       );
+    // case "select":
+    //   return (
+    //     <label>
+    //       <select id={id} value={value} onChange={onChange}>
+    //         {options.map((elem, i) => (
+    //           <option key={`option${i}`} value={elem}>
+    //             {elem}
+    //           </option>
+    //         ))}
+    //       </select>
+    //       {label}
+    //     </label>
+    //   );
+    case "checkbox":
+      return (
+        <label>
+          <input type="checkbox" id={id} value={value} onChange={onChange} />
+          {label}
+        </label>
+      );
+    // case "radio":
+    //   return (
+    //     <div
+    //       options={options.map((elem, i) => (
+    //         <label>
+    //           <input
+    //             type="radio"
+    //             id={`radio${i}`}
+    //             value={value}
+    //             onChange={onChange}
+    //           />
+    //           {label}
+    //         </label>
+    //       ))}
+    //     ></div>
+    //   );
   }
 }
